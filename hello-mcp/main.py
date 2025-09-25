cec2edbfad5692ca21df4d18c818fd9fee74014e
Ayush Kumar Giri <64836429+ayushpro0@users.noreply.github.com>
2025-09-25 21:11:40 +0530

    Update main.py

--- hello-mcp/main.py
+++ hello-mcp/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from hello-mcp!")
+from mcp.server.fastmcp import FastMCP
+from pydantic import Field
+
+mcp = FastMCP(
+    name="Hello MCP Server",
+    host="0.0.0.0",
+    port=3000,
+    stateless_http=True,
+    debug=False,
+)
+
+
+@mcp.tool(title="Welcome a User", description="Return a friendly welcome message for the user.")
+def welcome(name: str = Field(description="The name of the user")) -> str:
+    return f"Wecome {name} from this amazing application!"
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport="streamable-http")
